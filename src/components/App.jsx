8fea845f9cb5ac361a865d5fbb82e812649510d8
diana080299 <132782216+diana080299@users.noreply.github.com>
2023-11-25 19:50:37 +0000

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,69 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
+import { useState } from 'react';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const statistic = { good, neutral, bad };
 
-  onLeaveFeedback = option => {
-    this.setState(prevState => ({
-      [option]: prevState[option] + 1,
-    }));
-  };
-
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((total, count) => total + count, 0);
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    return (this.state.good / this.countTotalFeedback()) * 100;
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage =
-      this.countPositiveFeedbackPercentage().toFixed(0);
-    return (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          alignItems: 'center',
-          fontSize: 30,
-          border: '8px solid #00FFFF',
-        }}
-      >
-        <Section title="Please leave your feedback" />
-        <FeedbackOptions
-          options={['good', 'neutral', 'bad']}
-          onLeaveFeedback={this.onLeaveFeedback}
-        />
-        {total === 0 ? (
-          <Notification />
-        ) : (
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={positivePercentage}
-          />
-        )}
-      </div>
-    );
+  function onLeaveFeedback(option) {
+    switch (option) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   }
+
+  const countTotalFeedback = () => {
+    return Object.values(statistic).reduce((total, count) => total + count, 0);
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    return (good / countTotalFeedback()) * 100;
+  };
+
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage().toFixed(0);
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+        fontSize: 30,
+        border: '8px solid #00FFFF',
+      }}
+    >
+      <Section title="Please leave your feedback" />
+      <FeedbackOptions
+        options={Object.keys(statistic)}
+        onLeaveFeedback={onLeaveFeedback}
+      />
+      {total === 0 ? (
+        <Notification />
+      ) : (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          positivePercentage={positivePercentage}
+        />
+      )}
+    </div>
+  );
 }
